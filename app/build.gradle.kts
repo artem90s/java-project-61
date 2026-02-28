12bf2f30a2dc6277b53b833580a6205e3b4f8c06
javadev <loc90@rambler.ru>
2026-02-28 17:00:39 +0400

    first

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
